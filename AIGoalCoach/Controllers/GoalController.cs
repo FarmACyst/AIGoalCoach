ff3f9af9a5db547e07d6714011f69918dc0ed380
Syed Md Arbaz <Syed Md Arbaz@LAPTOP-4F5ETB8Q>
2026-03-14 12:36:23 +0530

    Add project files.

---
+++ AIGoalCoach/Controllers/GoalController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AIGoalCoach.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetGoals()
+        {
+            return Ok("Done");
+        }
+    }
+}
